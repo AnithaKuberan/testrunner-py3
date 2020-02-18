51aba7964a78c4adc68b2c328e2397356933713f
AnithaKuberan <41673804+AnithaKuberan@users.noreply.github.com>
2020-02-18 19:24:33 +0530

    Update AutoFailoverBaseTest.py

--- pytests/failover/AutoFailoverBaseTest.py
+++ pytests/failover/AutoFailoverBaseTest.py
@@ -532,6 +532,7 @@ class AutoFailoverBaseTest(BaseTestCase):
     }
 
     def _auto_failover_message_present_in_logs(self, ipaddress):
+        print(self.rest.get_logs(20))
         return any("Rebalance interrupted due to auto-failover of nodes ['ns_1@{0}'].".format(ipaddress) in
                    list(d.values()) for d in self.rest.get_logs(20))
 
